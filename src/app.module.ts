87f10363c41dd0ab5fe4869a3f015f14a1571b93
qingtu <liushuo@liushuodeMac-mini.local>
2021-08-10 16:54:11 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsController } from './cats/cats.controller';
+import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRootAsync({
+      useFactory: () => ({
+        typePaths: ['./**/*.graphql'],
+      }),
+    }),
+    CatsModule,
+  ],
+  controllers: [AppController, CatsController],
+  providers: [AppService, CatsService],
+})
+export class AppModule {}
