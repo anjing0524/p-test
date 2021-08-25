7ae3936cdb50c036893dae65839ece68565e24e1
qingtu <liushuo@liushuodeMac-mini.local>
2021-08-25 15:18:53 +0800

    feat: add csrf

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,20 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import helmet from 'fastify-helmet';
+import fastifyCsrf from 'fastify-csrf';
+import fastifyCookie from 'fastify-cookie';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const fastify = new FastifyAdapter({ logger: true });
+  fastify.register(helmet);
+  fastify.register(fastifyCookie);
+  fastify.register(fastifyCsrf);
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true }),
+    fastify,
   );
   await app.listen(4000);
 }
